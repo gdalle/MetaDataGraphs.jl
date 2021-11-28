7c5d803c7612e3faa7143cdcbf8ff048cd61833a
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2021-11-28 15:06:33 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.23

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using DataGraphs
+using Documenter
+
+DocMeta.setdocmeta!(DataGraphs, :DocTestSetup, :(using DataGraphs); recursive=true)
+
+makedocs(;
+    modules=[DataGraphs],
+    authors="Guillaume Dalle <22795598+gdalle@users.noreply.github.com> and contributors",
+    repo="https://github.com/gdalle/DataGraphs.jl/blob/{commit}{path}#{line}",
+    sitename="DataGraphs.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://gdalle.github.io/DataGraphs.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/gdalle/DataGraphs.jl",
+    devbranch="main",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,14 @@
+```@meta
+CurrentModule = DataGraphs
+```
+
+# DataGraphs
+
+Documentation for [DataGraphs](https://github.com/gdalle/DataGraphs.jl).
+
+```@index
+```
+
+```@autodocs
+Modules = [DataGraphs]
+```
