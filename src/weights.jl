03c935e101ed39ab53d4896ea5a7e8759b27a5b6
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2021-12-05 23:52:11 +0100

    Debug weights

--- src/weights.jl
+++ src/weights.jl
@@ -14,20 +14,15 @@ function get_edgeweight(
     return get_data(g, s, d).weight
 end
 
-function Graphs.weights(g::AbstractDataGraph{T,VL,VD,ED}) where {T,VL,VD,ED}
+function Graphs.weights(g::AbstractDataGraph{T}) where {T}
     n = nv(g)
-    I, J, V = Int[], Int[], ED[]
-    for edge in edges(g)
-        s, d = src(edge), dst(edge)
-        weight = get_edgeweight(g, s, d)
-        push!(I, s)
-        push!(J, d)
-        push!(V, weight)
-        if !is_directed(g)
-            push!(I, d)
-            push!(J, s)
-            push!(V, weight)
-        end
+    eds = edges(g)
+    I = [src(edge) for edge in eds]
+    J = [dst(edge) for edge in eds]
+    W = [get_edgeweight(g, src(edge), dst(edge)) for edge in eds]
+    if is_directed(g)
+        return sparse(I, J, W, n, n)
+    else
+        return sparse(vcat(I, J), vcat(J, I), vcat(W, W), n, n)
     end
-    return sparse(I, J, V, n, n)
 end
