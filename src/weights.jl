51094c356a57bf5c2ba03425f2d7fdf821865699
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2021-12-05 22:41:04 +0100

    Weights for edges of type NamedTuple

--- src/weights.jl
+++ src/weights.jl
@@ -1,22 +1,32 @@
-function Graphs.weights(
-    g::AbstractDataGraph{T,VL,VD,ED,GD}
-) where {T,G,VL,VD,ED<:Nothing,GD}
+function Graphs.weights(g::AbstractDataGraph{T,VL,VD,ED}) where {T,VL,VD,ED<:Nothing}
     return Graphs.DefaultDistance(nv(g))
 end
 
-function Graphs.weights(g::AbstractDataGraph{T,VL,VD,ED,GD}) where {T,VL,VD,ED<:Real,GD}
+function get_edgeweight(
+    g::AbstractDataGraph{T,VL,VD,ED}, s::Integer, d::Integer
+) where {T,VL,VD,ED<:Real}
+    return get_data(g, s, d)
+end
+
+function get_edgeweight(
+    g::AbstractDataGraph{T,VL,VD,ED}, s::Integer, d::Integer
+) where {T,VL,VD,ED<:NamedTuple}
+    return get_data(g, s, d).weight
+end
+
+function Graphs.weights(g::AbstractDataGraph{T,VL,VD,ED}) where {T,VL,VD,ED}
     n = nv(g)
     I, J, V = Int[], Int[], ED[]
     for edge in edges(g)
         s, d = src(edge), dst(edge)
-        data = get_data(g, s, d)
+        weight = get_edgeweight(g, s, d)
         push!(I, s)
         push!(J, d)
-        push!(V, data)
+        push!(V, weight)
         if !is_directed(g)
             push!(I, d)
             push!(J, s)
-            push!(V, data)
+            push!(V, weight)
         end
     end
     return sparse(I, J, V, n, n)
